3445902c95e5ca77767ce7d018dc603d4d1932d4
Inadeus <tianlangq@student.unimelb.edu.au>
2025-04-29 13:57:41 +0800

    创建了通用类

---
+++ LittleWitchStore/Assets/Scripts/Input/GameInputManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using GGG.Tool.Singleton;
+using UnityEngine;
+
+public class GameInputManager : Singleton<GameInputManager>
+{
+    
+}
